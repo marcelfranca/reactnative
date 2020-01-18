0e1bc817907c12220ac11c1c5fe523df45f8e88b
marcelfranca <marcel.arthur@live.com>
2020-01-18 18:38:46 -0300

    styles

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,48 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-        padding: 50,
-        flexDirection:'row',
-        width: '80%',
-        height: 300,
-        justifyContent: 'space-around',
-        alignItems: 'center'
-        }}
-    >
-        <View
-            style={{
-                backgroundColor: 'red',
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
-        >
-            <Text>1</Text>
-        </View>
-        <View
-            style={{
-                backgroundColor: 'green',
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
-        >
-            <Text>2</Text>
-        </View>
-        <View
-            style={{
-                backgroundColor: 'blue',
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
-        >
-            <Text>3</Text>
+    <View style={styles.screen}>
+        <View style={styles.inputContainer}>
+            <TextInput
+                placeholder="Course Goals"
+                style={styles.textInput}/>
+            <Button
+                title="Add"
+            />
         </View>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+    screen: {
+        padding: 50
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    textInput: {
+        width: '80%',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        padding: 10
+    }
+});
